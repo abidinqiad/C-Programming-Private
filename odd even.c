122acf98cd529a7cfb78501f686db4f0ba95c768
abidinqiad <abidinqiad@gmail.com>
2022-01-20 11:39:52 +0600

    odd even

---
+++ odd even.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+    int main(){
+
+    int number,result;
+    scanf("%d",&number);
+    result=number%2;
+    if (result==0){
+
+        printf("%d is even",number);
+    }else
+    {
+        printf("%d is odd",number);
+    }
+
+
+
+    return 0;
+    }
+
+
+
+
+
+
---
+++ reminder.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int value1=30;
+    int value2=23;
+
+    float result;
+
+    result = value1 % value2;
+
+
+    printf("%.2f",result);
+
+
+
+
+
+
+    return 0;
+}
