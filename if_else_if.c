3e1dcd1ed6bf9a2d449bae111fceeb2f6b4ec6d4
abidinqiad <abidinqiad@gmail.com>
2022-01-23 11:37:01 +0600

    while_loop done

--- if_else_if.c
+++ if_else_if.c
@@ -4,7 +4,7 @@
 int main()
 {
 
-    int mark=120, extra_cur = 10, govt_job = 2;
+    int mark=200, extra_cur = 10, govt_job = 2;
 
 
     if( mark>= 200)
@@ -12,19 +12,19 @@ int main()
 
         printf("Admitted");
     }
-    else if ( mark>= 180 && extra_cur == 10)
+    if ( mark>= 180 && extra_cur == 10)
     {
 
         printf("Admitted");
     }
 
-    else if ( mark>=150 && govt_job == 2){
+    if ( mark>=150 && govt_job == 2){
         printf("Admitted");
     }
 
-    else {
+   /* else {
         printf("Not Admitted");
-    }
+    }*/
 
 
 
---
+++ while_loop.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+
+int main()
+{
+
+    int i = 0,n;
+
+    scanf("%d",&n);
+
+
+    while(i<=n){
+
+       // printf("true\n");
+
+
+        printf("%d ",i);
+
+        i =  i + 1;
+
+
+
+
+    }
+
+
+
+    return 0;
+}
